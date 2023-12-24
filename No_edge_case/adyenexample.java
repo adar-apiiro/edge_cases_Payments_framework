11b3875b99a4bfc6739d9f28702cf8cb65964c24
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2023-12-24 17:53:10 +0200

    Update adyenexample.java

--- No_edge_case/adyenexample.java
+++ No_edge_case/adyenexample.java
@@ -10,43 +10,46 @@ import com.adyen.model.checkout.CheckoutPaymentsResponse;
 import com.adyen.model.checkout.RequestOptions;
 
 public class AdyenPaymentExample {
-    // Setup the client and service.
-    Client client = new Client("YOUR_API_KEY", Environment.TEST);
-    PaymentsApi paymentsApi = new PaymentsApi(client);
 
-    // Create a payment request.
-    CheckoutPaymentsRequest paymentRequest = new CheckoutPaymentsRequest();
-    paymentRequest.setMerchantAccount("YOUR_MERCHANT_ACCOUNT");
-    CardDetails cardDetails = new CardDetails();
-    cardDetails.encryptedCardNumber("test_4111111111111111")
-            .encryptedSecurityCode("test_737")
-            .encryptedExpiryMonth("test_03")
-            .encryptedExpiryYear("test_2030");
-    paymentRequest.setPaymentMethod(new CheckoutCard(cardDetails));
-    Amount amount = new Amount().currency("EUR").value(1000L);
-    paymentRequest.setAmount(amount);
-    paymentRequest.setReference("My first Adyen test payment with an API library/SDK");
-    paymentRequest.setReturnUrl("https://your-company.com/checkout?shopperOrder=12xy..");
+    public static void main(String[] args) {
+        // Setup the client and service.
+        Client client = new Client("YOUR_API_KEY", Environment.TEST);
+        PaymentsApi paymentsApi = new PaymentsApi(client);
 
-    // Add your idempotency key.
-    RequestOptions requestOptions = new RequestOptions();
-    requestOptions.setIdempotencyKey("YOUR_IDEMPOTENCY_KEY");
+        // Create a payment request.
+        CheckoutPaymentsRequest paymentRequest = new CheckoutPaymentsRequest();
+        paymentRequest.setMerchantAccount("YOUR_MERCHANT_ACCOUNT");
+        CardDetails cardDetails = new CardDetails();
+        cardDetails.encryptedCardNumber("test_4111111111111111")
+                .encryptedSecurityCode("test_737")
+                .encryptedExpiryMonth("test_03")
+                .encryptedExpiryYear("test_2030");
+        paymentRequest.setPaymentMethod(new CheckoutCard(cardDetails));
+        Amount amount = new Amount().currency("EUR").value(1000L);
+        paymentRequest.setAmount(amount);
+        paymentRequest.setReference("My first Adyen test payment with an API library/SDK");
+        paymentRequest.setReturnUrl("https://your-company.com/checkout?shopperOrder=12xy..");
 
-    // Make a request to the /payments endpoint.
-    CheckoutPaymentsResponse checkoutPaymentsResponse = paymentsApi.payments(paymentRequest, requestOptions);
+        // Add your idempotency key.
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.setIdempotencyKey("YOUR_IDEMPOTENCY_KEY");
 
-    // Additional payment-related methods (Add more as needed)
-    // For example:
-    // 1. Capture a payment
-    CaptureRequest captureRequest = new CaptureRequest();
-    captureRequest.setAmount(amount);
-    CaptureResponse captureResponse = paymentsApi.capture(checkoutPaymentsResponse.getPspReference(), captureRequest, requestOptions);
+        // Make a request to the /payments endpoint.
+        CheckoutPaymentsResponse checkoutPaymentsResponse = paymentsApi.payments(paymentRequest, requestOptions);
 
-    // 2. Refund a payment
-    RefundRequest refundRequest = new RefundRequest();
-    refundRequest.setAmount(amount);
-    RefundResponse refundResponse = paymentsApi.refund(checkoutPaymentsResponse.getPspReference(), refundRequest, requestOptions);
+        // Additional payment-related methods (Add more as needed)
+        // For example:
+        // 1. Capture a payment
+        CaptureRequest captureRequest = new CaptureRequest();
+        captureRequest.setAmount(amount);
+        CaptureResponse captureResponse = paymentsApi.capture(checkoutPaymentsResponse.getPspReference(), captureRequest, requestOptions);
 
-    // 3. Cancel a payment
-    CancelOrRefundResponse cancelResponse = paymentsApi.cancelOrRefund(checkoutPaymentsResponse.getPspReference(), requestOptions);
+        // 2. Refund a payment
+        RefundRequest refundRequest = new RefundRequest();
+        refundRequest.setAmount(amount);
+        RefundResponse refundResponse = paymentsApi.refund(checkoutPaymentsResponse.getPspReference(), refundRequest, requestOptions);
+
+        // 3. Cancel a payment
+        CancelOrRefundResponse cancelResponse = paymentsApi.cancelOrRefund(checkoutPaymentsResponse.getPspReference(), requestOptions);
+    }
 }
